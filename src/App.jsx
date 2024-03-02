f0bcfb37ab6213a8deb79c924a59f8af2dce4f3c
Shreyas-Ingale <shreyasingale09@gmail.com>
2024-03-02 11:17:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+function App() {
+  return (
+    <div className='bg-blue-600 text-slate-300'>
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
